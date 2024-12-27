4a23c8ac922a043a0dead1e0b2c912574fbae3db
akihiko.kawakami <kawakami130423@gmail.com>
2024-12-27 09:00:50 +0900

    Revise a bit

--- modules/initializer/main.tf
+++ modules/initializer/main.tf
@@ -3,24 +3,26 @@
 ########################
 
 # Define S3 Bucket for tfstate
-locals {
-  bucket = "terraform-tfstate-ssh-monitoring"
+variable "bucket" {
+  type = object({
+    bucket_name = string
+  })
 }
 
 resource "aws_s3_bucket" "tfstate" {
-  bucket = local.bucket
+  bucket = var.bucket.bucket_name
 }
 
 resource "aws_s3_bucket_versioning" "tfstate" {
-  bucket = local.bucket
-  
+  bucket = aws_s3_bucket.tfstate.bucket
+
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "tfstate" {
-  bucket = local.bucket
+  bucket = aws_s3_bucket.tfstate.bucket
 
   block_public_acls       = true
   block_public_policy     = true
@@ -29,7 +31,7 @@ resource "aws_s3_bucket_public_access_block" "tfstate" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
-  bucket = local.bucket
+  bucket = aws_s3_bucket.tfstate.bucket
 
   rule {
     apply_server_side_encryption_by_default {
